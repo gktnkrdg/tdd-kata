ed37d96cbe9efb3588ddf5287db37570767b0d07
gktnkrdg <gktnkrdg@gmail.com>
2020-10-04 02:56:30 +0300

    primefactor test

---
+++ src/PrimeFactor/PrimeFactorTest.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TDDKatas.PrimeFactor
+{
+    public class PrimeFactorTest
+    {
+        [Theory]
+        [InlineData(-100, new int[0])]
+        [InlineData(0, new int[0])]
+        [InlineData(5, new[] { 5 })]
+        [InlineData(10, new[] { 2,5 })]
+        [InlineData(50, new[] { 2, 5,5 })]
+        [InlineData(81, new[] { 3, 3, 3, 3 })]
+        [InlineData(100, new[] { 2, 2,5, 5 })]
+        [InlineData(240, new[] { 2, 2, 2, 2,3,5 })]
+        public void GetPrimeFactor(int number,int[] expectedArray)
+        {
+            PrimeFactor.GeneratePrimeFactors(number).Should().BeEquivalentTo(expectedArray);
+       }
+    }
+}
