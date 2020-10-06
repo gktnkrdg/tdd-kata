d7025a8abccc3765d301e239a5b8915dbd7abd22
gktnkrdg <gktnkrdg@gmail.com>
2020-10-07 02:16:02 +0300

    Leap year kata add

---
+++ src/LeapYear/LeapYear.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDKatas.LeapYear
+{
+    public class LeapYear
+    {
+        public bool IsLeapYear(int year)
+        {
+            return false;
+        }
+    }
+}
---
+++ src/LeapYear/LeapYearTest.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDKatas.LeapYear
+{
+   public class LeapYearTest
+    {
+       public void IsLeapYear_ShouldReturnYearIsALeapYear()
+        {
+            LeapYear leapYear = new LeapYear();
+
+            var isLeapYear = leapYear.IsLeapYear(2000);
+
+            isLeapYear.Should().Be(true);
+        }
+    }
+}
