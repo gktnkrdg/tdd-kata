e9f504ab2cc8d98230b9191d6bbe322429d2ff2d
gktnkrdg <gktnkrdg@gmail.com>
2020-10-07 23:10:40 +0300

    Tictactoe player wins in row check

---
+++ src/TicTacToeChecker/TicTacToe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TDDKatas.TicTacToeChecker
+{
+    public class TicTacToe
+    {
+      
+        public int CheckBoard(int[,] board)
+        {
+            var result = -1;
+            for (int i= 0; i < board.GetLength(1)   ; i++){
+                result = CheckRow(board.GetRow(i));
+                if (result > 0)
+                    break;
+            }
+            return result;
+        }
+
+
+        private int CheckRow(int [] boardRow)
+        {
+            return boardRow.Distinct().Count() == 1 ? boardRow[0] : -1;
+        }
+    
+    }
+}
---
+++ src/TicTacToeChecker/TicTacToeTest.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+using Xunit.Extensions;
+
+namespace TDDKatas.TicTacToeChecker
+{
+    public class TicTacToeTest
+    {
+
+
+        public static IEnumerable<object[]> PlayerWinsRowData
+        {
+            get
+            {
+                return new[]
+                {
+                    new object[] { new int[,] { { 1, 1, 1 }, { 1, 2, 1 }, { 2, 1, 1 } } , 1 },
+                    new object[] { new int[,] { { 1, 2, 1 }, { 2, 2, 2 }, { 2, 1, 1 } } , 2 },
+                    new object[] { new int[,] { { -1, -1, -1 }, { 1, 1, 1 }, { 2, 1, 1 } } , 1 },
+                };
+            }
+        }
+
+        [Theory]
+        [MemberData(nameof(PlayerWinsRowData))]
+
+        public void SolveBoard_PlayerWinsInRow_ShouldReturnPlayerWinner(int[,] board, int expectedResult)
+        {
+            TicTacToe ticTacToe = new TicTacToe();
+
+            var boardResult = ticTacToe.CheckBoard(board);
+
+            boardResult.Should().Be(expectedResult);
+        }
+       
+    }
+}
